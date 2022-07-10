d28c9350e12111c60f6429f6c417c141c4d1bf0c
jamesbell00 <james.bell@slu.edu>
2022-07-10 07:11:36 +0200

    xp changes

--- src/data/experience.js
+++ src/data/experience.js
@@ -21,13 +21,13 @@ const exp = [
         country: "USA",
         startDate: "Jan 2016",
         endDate: "Jul 2020",
-        description: `Go2Guys is targeted primarily towards the elderly and those with limited mobility by working miscellaneous labor jobs to make the customers' lives more convenient.
-        Grew company revenue by 400% compared to the first year by using advertisements in the local newspapers, social media, and community engagement.\nTrained and manage new hires; grew from 2 to 8 employees.`  
+        description: `Marketed towards the elderly and those with limited mobility by working miscellaneous labor jobs to make the customers' lives more convenient.
+        Grew company revenue by 400% compared to the first year by advertising in the local newspapers, social media, and our website.\nTrained and manage new hires; grew from 2 to 8 employees.`  
     },
     {
         id: 3,
         logo: "demandlocal.jpg",
-        title: "Data Analyst - Demand Local",
+        title: "Data Analyst",
         url: "https://www.demandlocal.com/",
         location: "Demand Local",
         city: "San Francisco",
@@ -35,9 +35,8 @@ const exp = [
         startDate: "Mar 2021",
         endDate: "Present",
         description: `Performed SQL queries with Python from the relational database to create reporting dashboards used by account managers and clients improving the reporting user interface for our products.
-                        Maintained data loader APIs
-                        Tools Used: SQL, Python, React, PHP, Sisense, Google & Facebook Ads
-                        Managed Google Analytics and Google Tag Manager accounts for clients.`  
+                        Maintained APIs and CRUD scripts
+                        Tools Used: SQL, Python, React, PHP, Sisense, Google & Facebook Ads`  
     }
 ]
 
